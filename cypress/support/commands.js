50aac09aa0f28961cd4fba0068ef82777bf3476c
Mateus Aniceto <mateuslaniceto@gmail.com>
2023-09-10 11:33:14 -0300

    feat: Adicionado Teste Listar Colaboradores

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,6 +1,7 @@
-import "../support/commands/ladingPage.commands";
+// import "../support/commands/ladingPage.commands";
 import '../support/commands/login.commands.js';
 // import '../support/commands/landingPage.commands.js';
 // import '@shelex/cypress-allure-plugin';
 import "../support/commands/adminHome.commands";
-import "../support/commands/adminHomeListarEmpresa.commands";
+// import "../support/commands/adminHomeListarEmpresa.commands";
+import "../support/commands/adminHomeListarColaboradores.commands";
