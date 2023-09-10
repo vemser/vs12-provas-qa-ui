f5b147671e71e97f48465c81b44ebc7a5166e603
holiveira93 <holiveira_93@hotmail.com>
2023-09-10 00:56:48 -0300

    branch pronta pra uso

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,3 +1,4 @@
 //import "../support/commands/landingPage.commands";
-import "../support/commands/adminHome.commands"
+//import "../support/commands/adminHome.commands"
+import "../support/commands/adminHomeListarCandidatos.commands"
 import '@shelex/cypress-allure-plugin';
