bcbc335e025f9a12583bfe8b51d0a4ab0b1bfff3
André Uchôa <andreuchoa2@gmail.com>
2023-09-10 11:22:09 -0300

    fix erro

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,3 +1,4 @@
 //import "../support/commands/landingPage.commands";
 import "../support/commands/adminHome.commands"
 import '@shelex/cypress-allure-plugin';
+import "../support/commands/adminHomeCadastrarEmpresa.commands"
