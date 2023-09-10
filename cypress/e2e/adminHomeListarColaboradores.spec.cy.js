ca4f2458bdbc874a8f6ac1ae3a37c29d16657e55
Mateus Aniceto <mateuslaniceto@gmail.com>
2023-09-10 11:49:29 -0300

    fix: Ajustado nome de teste listar colaboradores

--- cypress/e2e/adminHomeListarColaboradores.spec.cy.js
+++ cypress/e2e/adminHomeListarColaboradores.spec.cy.js
@@ -10,7 +10,7 @@ describe('AdminHomeListarColaboradores', () => {
     });
 
     
-    it('CT-WEB-029.1 - Validar "Listar Colaboradores" com sucesso', () => {
+    it('CT-WEB-028.1 - Validar "Listar Colaboradores" com sucesso', () => {
         cy.fixture('login.data.json').then(data => {
             cy.efetuarLogin(data.admGeral[0].email, data.admGeral[0].senha)
             cy.url().should('eq', 'http://vemser-dbc.dbccompany.com.br:39000/vemser/vs12-provas-front/dashboard')
