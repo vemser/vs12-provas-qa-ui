f43e0792b1fbafa9d582812c63de7f50748a7ad4
Lucas Lima <lucasdevs@protonmail.com>
2024-02-26 16:15:26 -0300

    init: projeto refatorado

--- cypress.config.js
+++ cypress.config.js
@@ -1,12 +1,9 @@
 const { defineConfig } = require("cypress");
-const allureWriter = require('@shelex/cypress-allure-plugin/writer');
 
 module.exports = defineConfig({
   e2e: {
-      setupNodeEvents(on, config) {
-          allureWriter(on, config);
-          return config;
-      },
-      "baseUrl":"http://vemser-dbc.dbccompany.com.br:39000/vemser/vs12-provas-front"
-  }
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
 });
