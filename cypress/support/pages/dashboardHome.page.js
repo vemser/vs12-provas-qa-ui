de00c400ebf78a50c9b6d012707e4c570b2a6909
Rodrigo Carvalho <rodrigofcc.dev@gmail.com>
2024-03-01 17:06:50 -0300

    add: dashboardHome page

---
+++ cypress/support/pages/dashboardHome.page.js
@@ -0,0 +1,18 @@
+// Textos
+let tituloH2Inicio = '#root > section > main > header > h2'
+let tituloH3Cadastros = '#root > section > main > section.sc-jdkVqZ.yYmgc > h3:nth-child(1)'
+let tituloH3Listagens = '#root > section > main > section.sc-jdkVqZ.yYmgc > h3:nth-child(3)'
+
+// Botões Cadastro
+let btnColaboradorCadastro = '#root > section > main > section.sc-jdkVqZ.yYmgc > div:nth-child(2) > button:nth-child(1)'
+let btnCandidatoCadastro ='#root > section > main > section.sc-jdkVqZ.yYmgc > div:nth-child(2) > button:nth-child(2)'
+let btnEmpresaCadastro ='#root > section > main > section.sc-jdkVqZ.yYmgc > div:nth-child(2) > button:nth-child(3)'
+let btnQuestoesCadastro ='#root > section > main > section.sc-jdkVqZ.yYmgc > div:nth-child(2) > button:nth-child(4)'
+let btnProcessoSeletivoCadastro ='#root > section > main > section.sc-jdkVqZ.yYmgc > div:nth-child(2) > button:nth-child(5)'
+
+// Botões Listagem
+let btnColaboradorListagem = '#root > section > main > section.sc-jdkVqZ.yYmgc > div:nth-child(4) > button:nth-child(1)'
+let btnCandidatoListagem ='#root > section > main > section.sc-jdkVqZ.yYmgc > div:nth-child(4) > button:nth-child(2)'
+let btnEmpresaListagem ='#root > section > main > section.sc-jdkVqZ.yYmgc > div:nth-child(4) > button:nth-child(3)'
+let btnQuestoesListagem ='#root > section > main > section.sc-jdkVqZ.yYmgc > div:nth-child(4) > button:nth-child(4)'
+let btnProcessoSeletivoListagem ='#root > section > main > section.sc-jdkVqZ.yYmgc > div:nth-child(4) > button:nth-child(5)'
