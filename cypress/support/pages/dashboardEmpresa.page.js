7618011dfece2b1850d5108a29de5a7c7183c15d
Rodrigo Carvalho <rodrigofcc.dev@gmail.com>
2024-03-01 17:17:05 -0300

    add: dashboardEmpresa page

---
+++ cypress/support/pages/dashboardEmpresa.page.js
@@ -0,0 +1,12 @@
+// Textos
+let tituloH2Empresa = '#root > section > main > header > h2'
+let tituloH3Cadastros = '#root > section > main > section > h3:nth-child(1)'
+let tituloH3Listagens = '#root > section > main > section > h3:nth-child(3)'
+
+// Botões Cadastro
+let btnColaboradorCadastro = '#root > section > main > section > div:nth-child(2) > button:nth-child(1)'
+let btnQuestoesCadastro ='#root > section > main > section > div:nth-child(2) > button:nth-child(2)'
+let btnProcessoSeletivoCadastro ='#root > section > main > section > div:nth-child(2) > button:nth-child(3)'
+
+// Botões Listagem
+let btnColaboradorListagem = '#root > section > main > section > div:nth-child(4) > button'
