1c1c1581c12d9173d8f360f5cf20695a3e250bc5
Lucas Lima <lucasdevs@protonmail.com>
2024-02-29 22:24:56 -0300

    fix: capture

--- capture.js
+++ capture.js
@@ -15,13 +15,24 @@ async function captureScreenshotAndUpload() {
     });
     await page.goto(`https://deep-sunbird-heroic.ngrok-free.app/login`);
     await new Promise(resolve => setTimeout(resolve, 15000));
-    await page.click('#root > div > main > div > div > section.mb-4.border.border-gray-300.bg-white.drop-shadow-md > div > footer > button');
+    // clicar no botão do ngrok
+    try {
+        await page.click('#root > div > main > div > div > section.mb-4.border.border-gray-300.bg-white.drop-shadow-md > div > footer > button');
+    } catch (error) {
+        console.log(error)
+    }
     await new Promise(resolve => setTimeout(resolve, 5000));
-    await page.type('#j_username', 'admin')
-    await page.type('#j_password', 'admin')
-    await page.click('#main-panel > div > form > button')
+    // tenta fazer login no jenkins
+    try {
+        await page.type('#j_username', 'admin')
+        await page.type('#j_password', 'admin')
+        await page.click('#main-panel > div > form > button')
+    } catch (error) {
+        console.log(error)
+    }
+
     await new Promise(resolve => setTimeout(resolve, 5000));
-    await page.goto(`https://deep-sunbird-heroic.ngrok-free.app/job/PIPE/47/allure/`);
+    await page.goto(`https://deep-sunbird-heroic.ngrok-free.app/job/PIPE/${buildNumber}/allure/`);
     await new Promise(resolve => setTimeout(resolve, 5000));
     await page.screenshot({
         path: 'screenshot.png'
