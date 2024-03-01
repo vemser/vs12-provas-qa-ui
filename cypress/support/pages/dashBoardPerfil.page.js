4031d1385a82c43dcb0edd833e61af3a2b849bc7
Lucas Lima <lucasdevs@protonmail.com>
2024-03-01 17:02:08 -0300

    feat: perfil & prova page

---
+++ cypress/support/pages/dashBoardPerfil.page.js
@@ -0,0 +1,11 @@
+import { faker } from '@faker-js/faker';
+
+// seção configurações do perfil
+let inputNomeCompleto = ''
+let inputEmail = ''
+let inputSenha = ''
+let btnVisibilidadeSenha = ''
+let btnEditar = ''
+let btnCancelar = ''
+
+let popEditarSucesso = '#\\31  > div.Toastify__toast-body > div:nth-child(2)'
---
+++ cypress/support/pages/dashboardProvas.page.js
@@ -0,0 +1,12 @@
+import { faker } from '@faker-js/faker';
+
+// seção de cadastros
+let btnQuestoesCadastros = ''
+let btnProcessoSeletivo = ''
+
+// seção de listagens
+let btnQuestoesListagens = ''
+
+Cypress.Commands.add('x', () => {
+
+})
